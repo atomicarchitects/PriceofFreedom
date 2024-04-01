bc1f49e8881295869b1824bed6b9aaa7dfcf3264
Mit Kotak <mitkotak0305@gmail.com>
2024-04-01 14:57:15 -0400

    hidden_lmax change

--- shell/run_timing.sh
+++ shell/run_timing.sh
@@ -4,7 +4,7 @@ for LMAX in 3 4 5 6 7 8
 do	
     for TPTYPE in "usual" "gaunt" "vectorgaunt"
     do
-        python ../tetris.py --sh_lmax=$LMAX --tensor_product_type=$TPTYPE
+        python ../tetris.py --sh_lmax=$LMAX --hidden_lmax=$LMAX --tensor_product_type=$TPTYPE
     done
 done
 
